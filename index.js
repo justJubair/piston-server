90f4665f16db6ed30f67b2c50195a24c77992d91
Jubair <jubair.ahmed2838@gmail.com>
2023-10-18 01:26:05 +0600

    Add: product post endpoint added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require("express")
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require("cors")
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-const uri = "mongodb+srv://piston:VDzwVeJJQqBOvZ4S@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://piston:VDzwVeJJQqBOvZ4S@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,27 +25,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-	const pistonCollection = client.db("pistonDB").collection("brands")
-    const productsCollection = client.db("pistonDB").collection("producst")
+  
+    const productsCollection = client.db("pistonDB").collection("products");
 
-   
+    // product post endpoint
+    app.post("/products", async(req, res)=>{
+      const product = req.body;
+      const result = await productsCollection.insertOne(product)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-app.get("/", (req, res)=>{
-    res.send("Piston server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Piston server is running");
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
 
 // VDzwVeJJQqBOvZ4S
 // piston

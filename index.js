824cf08a020ebc57b42040b57dc501d3156cbb1a
Jubair <jubair.ahmed2838@gmail.com>
2023-10-18 21:41:08 +0600

    Add: get a single product endpoint added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const tesla = require("./tesla.json")
-const mclaren = require("./mclaren.json")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const tesla = require("./tesla.json");
+const mclaren = require("./mclaren.json");
 const benz = require("./benz.json");
-const lambo = require("./lambo.json")
-const ferrari= require("./ferrari.json")
-const bmw = require("./bmw.json")
+const lambo = require("./lambo.json");
+const ferrari = require("./ferrari.json");
+const bmw = require("./bmw.json");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,73 +31,84 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-  
     const productsCollection = client.db("pistonDB").collection("products");
-    const brandsCollection = client.db("pistonDB").collection("brands")
-
+    const brandsCollection = client.db("pistonDB").collection("brands");
 
     // brands get endpoint
-    app.get("/brands", async(req, res)=>{
+    app.get("/brands", async (req, res) => {
       const cursor = brandsCollection.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
+
     // brands post endpoint
-    app.post("/brands", async(req, res)=>{
+    app.post("/brands", async (req, res) => {
       const brand = req.body;
-      const result = await brandsCollection.insertOne(brand)
-      res.send(result)
-    })
+      const result = await brandsCollection.insertOne(brand);
+      res.send(result);
+    });
 
+    // get products endpoint
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get a single product
+    app.get("/products/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    });
     // products post endpoint
-    app.post("/products", async(req, res)=>{
+    app.post("/products", async (req, res) => {
       const product = req.body;
-      const result = await productsCollection.insertOne(product)
-      res.send(result)
-    })
-   
-    // specific brand product get endpoint tesla
-    app.get("/tesla", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"Tesla"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
 
+    // specific brand product get endpoint tesla
+    app.get("/tesla", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "Tesla" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Mclaren
-    app.get("/mclaren", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"McLaren"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/mclaren", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "McLaren" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // mercedes benz
-    app.get("/marcedes", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"Mercedes-Benz"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/marcedes", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "Mercedes-Benz" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // lamborghini
-    app.get("/lamborghini", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"Lamborghini"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/lamborghini", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "Lamborghini" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // ferrari
-    app.get("/ferrari", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"Ferrari"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/ferrari", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "Ferrari" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // bmw
-    app.get("/bmw", async(req, res)=>{
-      const cursor = productsCollection.find({"brand":"BMW"})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-   
+    app.get("/bmw", async (req, res) => {
+      const cursor = productsCollection.find({ brand: "BMW" });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

41339b291bca547f95299ce747cd3628e935a8bc
Jubair <jubair.ahmed2838@gmail.com>
2023-10-19 14:10:03 +0600

    Add: dotenv file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+require('dotenv').config()
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://piston:VDzwVeJJQqBOvZ4S@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productsCollection = client.db("pistonDB").collection("products");
     const brandsCollection = client.db("pistonDB").collection("brands");

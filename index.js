09f6d2f67afdb702160398a38d79a210cf1cac9f
Jubair <jubair.ahmed2838@gmail.com>
2023-10-20 13:59:20 +0600

    Add: deployed to vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productsCollection = client.db("pistonDB").collection("products");
     const brandsCollection = client.db("pistonDB").collection("brands");
